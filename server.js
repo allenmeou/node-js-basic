1a5906f73592f687189b389ed9c5b8d09549e2c8
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-08-26 09:47:43 +0700

    upload code

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World! vs meou");
+});
+app.get("/about", (req, res) => {
+  res.send(`I'm Eric!`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
