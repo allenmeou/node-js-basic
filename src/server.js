b37ae19cd4a2f9343734decbf1ac7387827e3ced
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-08-29 10:41:20 +0700

    upload code #7 --static file

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./index.html"));
+console.log(">>> check port:", port);
+
+configViewEngine(app);
+
+app.get("/", (req, res) => {
+  res.render("test/index.ejs");
 });
-
 app.get("/about", (req, res) => {
-  res.send(`I'm MeoU!`);
+  res.send(`I'm Eric!`);
 });
 
 app.listen(port, () => {
